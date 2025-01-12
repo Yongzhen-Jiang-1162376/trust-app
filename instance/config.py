d9bd404f719c687df29025a108ff44901e2246b6
Yongzhen (Larry) Jiang <yongzhen.jiang@gmail.com>
2025-01-13 01:43:31 +1300

    change env file

--- instance/config.py
+++ instance/config.py
@@ -2,7 +2,9 @@ import os
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv()
+
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+load_dotenv(dotenv_path)
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
