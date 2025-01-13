6396192957bd56964c91f1c19558f7b81421a685
Yongzhen (Larry) Jiang <yongzhen.jiang@gmail.com>
2025-01-14 11:23:33 +1300

    fix sql

--- sql/tables-update.sql
+++ sql/tables-update.sql
@@ -1,4 +1,4 @@
 ALTER TABLE hr_employee
-MODIFY COLUMN hours_per_week varcharz(255) NULL DEFAULT NULL;
+MODIFY COLUMN hours_per_week varchar(255) NULL DEFAULT NULL;
 ALTER TABLE hr_employee
-MODIFY COLUMN trial_period varcharz(255) NULL DEFAULT NULL;
+MODIFY COLUMN trial_period varchar(255) NULL DEFAULT NULL;
