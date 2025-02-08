a4c300e872f904cb9b7feb61c4872ab62dfbd9d4
Yongzhen (Larry) Jiang <yongzhen.jiang@gmail.com>
2025-02-08 13:41:40 +1300

    add sql

---
+++ sql/updates.sql
@@ -0,0 +1,9 @@
+INSERT INTO auth_user (email, PASSWORD, full_name, is_active, is_blocked, is_superadmin)
+VALUES ('rachel.witana@tiakitaongatrust.co.nz', 
+		  'scrypt:32768:8:1$XhI1ikIgExfpRKKT$2bcbc944f38c1d5c3245f53e39a830e205944bb629437859f15dee145b150e3641e931990931832d772e8bb36bed9aff694dc9c3d239dd5468ca973865478e53',
+		  'Rachel Witana',
+		  1,
+		  0,
+		  1
+		 )
+;
